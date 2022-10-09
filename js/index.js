d03672e26d205769f6ffbb0bdf17f3a73934e3c4
SuvitTMB <82767838+SuvitTMB@users.noreply.github.com>
2022-10-09 09:58:31 +0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ var sResultLogin ="ไม่สำเร็จ"
 
 
 $(document).ready(function () {
-
+/*
   sessionStorage.clear(); 
   var str = "";
   var sLineID = "Ua6b6bf745bd9bfd01a180de1a05c23b3";
@@ -18,14 +18,14 @@ $(document).ready(function () {
   str += '<div class="NameLine">'+ sessionStorage.getItem("LineName")+'</div>';
   $("#MyProfile").html(str);  
   Connect_DB();
-    
-  //main();
+*/    
+  main();
 });
 
 
 
 async function main() {
-  await liff.init({ liffId: "1655966947-EjZeO6yr" });
+  await liff.init({ liffId: "1657509542-pqGkM6l2" });
   document.getElementById("isLoggedIn").append(liff.isLoggedIn());
   if(liff.isLoggedIn()) {
     getUserProfile();
